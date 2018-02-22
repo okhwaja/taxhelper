136ba1a88cdcbbcd7baa4ddb1d80b2a038be5c34
okhwaja <osman.khwaja@gmail.com>
2018-02-22 08:42:27 -0800

    cleanup

--- taxhelper/__main__.py
+++ taxhelper/__main__.py
@@ -4,7 +4,6 @@ import argparse
 def main():
 
   parser = argparse.ArgumentParser(description='TurboTax Helper')
-  parser.add_argument('--service', type=str, help='The ECS service to connect to. ({})'.format(',\n'.join(services.keys())), required=True)
   parser.add_argument('--trades', type=str, help='path to CSV file of trades', required=True)
   parser.add_arguments('--credentials', type=str, help='path to JSON credentials file', required=True)
   parser.add_argument('--start-index', type=int, help='Begin at the index of trade in trades CSV. Useful for when bot gets stuck and want to resume without starting over', default=0)
