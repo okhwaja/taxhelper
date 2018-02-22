37ff6ae4fa6b76dd48a91ae4aa771577ad81a8ba
okhwaja <osman.khwaja@gmail.com>
2018-02-22 09:19:55 -0800

    add dummy csvs

--- taxhelper/__main__.py
+++ taxhelper/__main__.py
@@ -5,7 +5,7 @@ def main():
 
   parser = argparse.ArgumentParser(description='TurboTax Helper')
   parser.add_argument('--trades', type=str, help='path to CSV file of trades', required=True)
-  parser.add_arguments('--credentials', type=str, help='path to JSON credentials file', required=True)
+  parser.add_argument('--credentials', type=str, help='path to JSON credentials file', required=True)
   parser.add_argument('--start-index', type=int, help='Begin at the index of trade in trades CSV. Useful for when bot gets stuck and want to resume without starting over', default=0)
 
   args = parser.parse_args()
